cdfb851ea1e0802a96f702469c28c4400a231cbc
Tallis Vinicius <tallis.vinicius03@gmail.com>
2025-12-01 20:37:49 -0300

    Treinando git

---
+++ modulo-01-introducao/src/Ex002.java
@@ -0,0 +1,8 @@
+public class Ex002 {
+    public static void main(String[] args) {
+        String nome = "Tallis ";
+        int idade = 26;
+        System.out.println("Sr " + nome + ". Seja bem vindo ao mundo da programação.");
+        System.out.println("Eu tenho " + idade + " anos, faço faculdade de Ciência da Computação.");
+    }
+}
